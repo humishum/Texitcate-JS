29214f8d17de3d5155b92c4ed5fa95c7bfc4f98b
Humdaan Mustafa <humishum@gmail.com>
2015-08-25 12:26:52 -0700

    test

---
+++ app.js
@@ -0,0 +1,9 @@
+var client = require('twilio')(accountSid, authToken);
+ 
+client.messages.create({
+    body: "Testing Heroku",
+    to: "+16303120721",
+    from: "+12243658561"
+}, function(err, message) {
+    process.stdout.write(message.sid);
+});
