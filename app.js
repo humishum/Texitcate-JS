0d4af50fe4a7f2d9826da0f044e80b3b88d0e7eb
Humdaan Mustafa <humishum@gmail.com>
2015-08-26 20:44:43 -0700

    semicolon lol

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var client = require('twilio')(accountSid, authToken);
 client.messages.create({
     body: "Testing Heroku",
     to: "+16303120721",
-    from: "+12243658561"
+    from: "+12243658561";
 }, function(err, message) {
     process.stdout.write(message.sid);
 });
